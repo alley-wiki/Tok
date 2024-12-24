630f9a7f60d3e3b7f83ffa6cdac4a6f6cad1f099
alley-wiki <rocker4782@gmail.com>
2024-12-24 23:22:50 +0500

    first commit

--- src/components/WikiPage.jsx
+++ src/components/WikiPage.jsx
@@ -7,12 +7,12 @@ export default function WikiPage() {
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const rockerInfo = {
-    name: "Рокер",
-    nickname: "rocker7003",
-    joinDate: "08.04.2023",
-    age: "16 лет",
-    birthDate: "17.11.08",
-    birthPlace: "Казахстан"
+    name: "Ток",
+    nickname: "tok_69",
+    joinDate: "10.08.2022",
+    age: "15",
+    birthDate: "06.09.2009",
+    birthPlace: "Сибирь"
   };
 
   useEffect(() => {
@@ -59,47 +59,64 @@ export default function WikiPage() {
             <div className="main-info section">
               <h2>Основная информация</h2>
               <p>
-                Рокер (rocker7003) - зашел на сервер 8 апреля 2023 года, найдя видео 
-                <a href="/Cherru" className="link"> Черр </a> 
-                в рекомендациях ютуба, в котором ему понравился стиль рисовки, и потом перейдя 
-                по приглашению на сервер из описания видео.
+                Ток (tok_69) - участник 
+                <a href="https://discord.gg/dRu68ERWFx" className="link"> Discord-сервера Вишневых Аллей</a>, зашел на сервер 10 августа 2022 года, увидев в рекомендациях творчество 
+                <a href="/Cherru" className="link"> Черр</a>, а именно такие видео, как 
+                "<a href="https://youtu.be/88BYU18NSYQ?si=7CP50hDSjklWnrH4" className="link">Аскорбинка | animation (ft.Lololoshka, Кавински) Идеальный мир</a>" и 
+                "<a href="https://youtu.be/hpwbFvcqIjo?si=9lcM9lesrm_FBf8U" className="link">45 секунд шуток про руку (Идеальный мир) ft.Кейт, Радан, Райя</a>". 
+                В дальнейшем, начал следить за творчеством 
+                <a href="/Cherru" className="link"> Черр</a>.
               </p>
 
-              <h2>Чем занимается</h2>
-              <p>Общается на сервере, делает ии-каверы и редактирует эту википедию.</p>
+              <h2>Чем занимался после захода на сервер и причины бана</h2>
+              <p>
+                В сентябре ему банят первый аккаунт. 14 сентября 2022 года, он создаёт другой аккаунт. Этот аккаунт держится до 29-го сентября и улетает в бан. 
+                Ныне больше не используется. Создаётся 3 аккаунт, который он использует сейчас.
+              </p>
+              <p>
+                Причины бана: Хорни поведение, 5.2, оскорбление и конфликты с участниками.
+              </p>
+
+              <h2>Разбан</h2>
+              <p>
+                Проходит 370 дней с 29 сентября 2022 года, с последнего появления Тока, пока его не разбанивают. Ночью с 10 по 11 декабря 2023 года он ливает т.к. по его мнению 
+                <a href="https://visneviye-allei-wiki.github.io/sai/" className="link"> Сай</a> устраивает раздачу мутов и предов всем подряд. Позже, а именно днём 11 числа, Tok возвращается обратно на 
+                <a href="https://discord.gg/dRu68ERWFx" className="link"> сервер</a>.
+              </p>
+
+              <h2>Чем занимается сейчас</h2>
+              <p>
+                Сейчас он общается с людьми, но редко.
+              </p>
 
               <h2>Интересные факты</h2>
               <ul>
-                <li>Создал эту википедию, но была старая на фэндоме, но её удалили</li>
-                <li>Часто меняет аватарки и никнеймы</li>
-                <li>Иногда притворяется кем-то ради шутки</li>
+                <li>Находится в браке с Рейтором больше 60 дней</li>
+                <li>Живет в Сибири</li>
+                <li>Ник в бравле: про</li>
               </ul>
             </div>
 
             <div className="infobox section">
               <h2>{rockerInfo.name}</h2>
-              <img src={avatar} alt="Рокер" className="avatar" />
+              <img src={avatar} alt="Ток" className="avatar" />
               <table>
                 <tbody>
                   <tr>
-                    <td>Никнейм</td>
-                    <td>{rockerInfo.nickname}</td>
-                  </tr>
-                  <tr>
-                    <td>Дата захода на сервер</td>
-                    <td>{rockerInfo.joinDate}</td>
+                    <td>Должность</td>
+                    <td>Участник</td>
                   </tr>
                   <tr>
                     <td>Возраст</td>
                     <td>{rockerInfo.age}</td>
                   </tr>
                   <tr>
-                    <td>Дата рождения</td>
-                    <td>{rockerInfo.birthDate}</td>
+                    <td>Дата захода на сервер</td>
+                    <td>{rockerInfo.joinDate}</td>
                   </tr>
                   <tr>
-                    <td>Место рождения</td>
-                    <td>{rockerInfo.birthPlace}</td>
+                    <td>Дата рождения</td>
+                    <td>{rockerInfo.birthDate}</td>
                   </tr>
                 </tbody>
               </table>
